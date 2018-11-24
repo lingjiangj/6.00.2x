0f6f404a53e0a542d4d45f4683e3fa4d41af964c
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-24 14:42:55 +0800

    Update Problem 7.py

--- Quiz/Problem 7.py
+++ Quiz/Problem 7.py
@@ -4,14 +4,10 @@ def solveit(test):
         Returns an int, x, with the smallest absolute value such that test(x) is True 
         In case of ties, return any one of them. 
     """
-    # IMPLEMENT THIS FUNCTION
-
-#### This test case prints 49 ####
-def f(x):
-    return (x+15)**0.5 + x**0.5 == 15
-print(solveit(f))
-
-#### This test case prints 0 ####
-def f(x):
-    return x == 0
-print(solveit(f))
+    i = 0
+    while True:
+        for sign in (1, -1):
+            x = i * sign
+            if test(x):
+                return x
+        i += 1

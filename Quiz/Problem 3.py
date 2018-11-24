8033ffb5e39c33ea664bc66d3c2ff2540a2078c2
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-24 13:18:59 +0800

    Update Problem 3.py

--- Quiz/Problem 3.py
+++ Quiz/Problem 3.py
@@ -9,3 +9,28 @@ def song_playlist(songs, max_size):
     Returns: a list of a subset of songs fitting in 'max_size' in the order 
              in which they were chosen.
     """
+    
+    playlist = []
+    remainSpace = max_size
+    # start with the first song in the given song list. If the first song doesn't fit on disk, return an empty list. 
+    if songs[0][2] <= max_size:
+        playlist.append(songs[0][0])
+        remainSpace -=songs[0][2]
+        # sort the songslist
+        songsList = sorted(songs, key = lambda x :x[2])
+        # remove the first song in songs
+        songsList.remove(songs[0])
+    
+        for song in songsList:
+            if song[2] <=remainSpace:
+                playlist.append(song[0])
+                remainSpace -=song[2]
+            else:
+                break
+    # if the first song in songs dose not fit the disk, return empty list
+    else:
+        playlist = []        
+        
+    return playlist
+
+

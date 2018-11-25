4e368ebc3fb4385bcf8c792ca156d352f1d31459
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-25 12:38:21 +0800

    Update Lecture2-Exercise 1.py

--- Exercises/Lecture2-Exercise 1.py
+++ Exercises/Lecture2-Exercise 1.py
@@ -6,3 +6,15 @@ def yieldAllCombos(items):
       Yields a tuple, (bag1, bag2), where each bag is represented as 
       a list of which item(s) are in each bag.
     """
+    N = len(items)
+    # enumerate the **N possible combinations
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i//(j*3)) % 3 == 1:
+                bag1.append(items[j])
+            elif (i//(j*3)) % 3 == 2:
+                bag2.append(items[j])
+        yield(bag1,bag2)
